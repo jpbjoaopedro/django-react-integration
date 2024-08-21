8649c205ca0c8e5d1cb1b087385b593292904996
jpbjoaoepedro <jpbandeiradossantos@gmail.com>
2024-08-21 13:59:44 -0300

    First commit

---
+++ backend/core/views.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions, viewsets, authentication
+from .serializers import ListSerializer, ItemSerializer
+from .models import List, Item
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
