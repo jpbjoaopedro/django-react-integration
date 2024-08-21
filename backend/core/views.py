fc45f7d8aeb1f31bb99edea2924daa1c1adde43d
jpbjoaoepedro <jpbandeiradossantos@gmail.com>
2024-08-21 17:26:22 -0300

    Adding token and additional headers to the fetch call

--- backend/core/views.py
+++ backend/core/views.py
@@ -5,8 +5,8 @@ from .models import List, Item
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
-    # permission_classes = [permissions.IsAuthenticated]
-    # authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
--- frontend/src/UserLists.js
+++ frontend/src/UserLists.js
@@ -5,8 +5,15 @@ export default class UserLists extends React.Component{
     state = { lists: null, loading: false }
 
     async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 4e942231525d69407c9b5cb801969b50269f59fa'
+
         var url = 'http://127.0.0.1:8000/list/';
-        const response = await fetch(url);
+        const response = await fetch(url, config);
         const data = await response.json();
         console.log(data);
         this.setState({lists: data, loading: false})
